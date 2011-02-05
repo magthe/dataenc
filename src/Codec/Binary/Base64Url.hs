0326366534515d640971679c6d5a8efe9b77aeca
Magnus Therning <magnus@therning.org>
2011-02-05 20:04:50 +0000

    Re-writing base64url decoding in incremental style.

--- src/Codec/Binary/Base64Url.hs
+++ src/Codec/Binary/Base64Url.hs
@@ -9,43 +9,116 @@
 -- <http://www.haskell.org/haskellwiki/Library/Data_encoding>.
 module Codec.Binary.Base64Url
     ( encode
+    , DecIncData(..)
+    , DecIncRes(..)
+    , decodeInc
     , decode
-    , decode'
     , chop
     , unchop
     ) where
 
 import Data.Maybe
 import Data.Word
+import Data.Bits
+import Data.Array
+import qualified Data.Map as M
 
 import qualified Codec.Binary.Base64 as Base64
+
+-- {{{1 enc/dec map
+_encMap =
+    [ (0, 'A'), (1, 'B'), (2, 'C'), (3, 'D'), (4, 'E')
+    , (5, 'F') , (6, 'G'), (7, 'H'), (8, 'I'), (9, 'J')
+    , (10, 'K'), (11, 'L'), (12, 'M'), (13, 'N'), (14, 'O')
+    , (15, 'P'), (16, 'Q'), (17, 'R'), (18, 'S'), (19, 'T')
+    , (20, 'U'), (21, 'V'), (22, 'W'), (23, 'X'), (24, 'Y')
+    , (25, 'Z'), (26, 'a'), (27, 'b'), (28, 'c'), (29, 'd')
+    , (30, 'e'), (31, 'f'), (32, 'g'), (33, 'h'), (34, 'i')
+    , (35, 'j'), (36, 'k'), (37, 'l'), (38, 'm'), (39, 'n')
+    , (40, 'o'), (41, 'p'), (42, 'q'), (43, 'r'), (44, 's')
+    , (45, 't'), (46, 'u'), (47, 'v'), (48, 'w'), (49, 'x')
+    , (50, 'y'), (51, 'z'), (52, '0'), (53, '1'), (54, '2')
+    , (55, '3'), (56, '4'), (57, '5'), (58, '6'), (59, '7')
+    , (60, '8'), (61, '9'), (62, '-'), (63, '_') ]
+
+-- {{{1 encodeArray
+encodeArray :: Array Word8 Char
+encodeArray = array (0, 64) _encMap
+
+-- {{{1 decodeMap
+decodeMap :: M.Map Char Word8
+decodeMap  = M.fromList [(snd i, fst i) | i <- _encMap]
 
 -- {{{1 encode
 -- | Encode data.
 encode :: [Word8]
     -> String
-encode os = let
-        swapUrlChar c = case c of
-            '+' -> '-'
-            '/' -> '_'
-            _ -> c
-    in map swapUrlChar $ Base64.encode os
+encode = let
+        pad n = replicate n 0
+        enc [] = ""
+        enc l@[o] = (++ "==") . take 2 .enc $ l ++ pad 2
+        enc l@[o1, o2] = (++ "=") . take 3 . enc $ l ++ pad 1
+        enc (o1 : o2 : o3 : os) = let
+                i1 = o1 `shiftR` 2
+                i2 = (o1 `shiftL` 4 .|. o2 `shiftR` 4) .&. 0x3f
+                i3 = (o2 `shiftL` 2 .|. o3 `shiftR` 6) .&. 0x3f
+                i4 = o3 .&. 0x3f
+            in foldr ((:) . (encodeArray !)) "" [i1, i2, i3, i4] ++ enc os
+    in enc
 
 -- {{{1 decode
--- | Decode data (lazy).
-decode' :: String
-    -> [Maybe Word8]
-decode' = let
-        unSwapUrlChar c = case c of
-            '-' -> '+'
-            '_' -> '/'
-            _ -> c
-    in Base64.decode' . map unSwapUrlChar
+-- | Decode data (strict).
+data DecIncData = Chunk String | Done
+data DecIncRes = Part [Word8] (DecIncData -> DecIncRes) | Final [Word8] String | Fail [Word8] String
+
+-- | Decode data incrementally by passing chunks.
+decodeInc :: DecIncData
+    -> DecIncRes
+decodeInc d = dI [] d
+    where
+        dec4 cs = let
+                ds = map (flip M.lookup decodeMap) cs
+                es@[e1, e2, e3, e4] = map fromJust ds
+                o1 = e1 `shiftL` 2 .|. e2 `shiftR` 4
+                o2 = e2 `shiftL` 4 .|. e3 `shiftR` 2
+                o3 = e3 `shiftL` 6 .|. e4
+                allJust = and . map isJust
+            in if allJust ds
+                then Just [o1, o2, o3]
+                else Nothing
+
+        dI [] (Done) = Final [] []
+        dI lo (Done) = Fail [] lo
+        dI lo (Chunk s) = doDec [] (lo ++ s)
+            where
+                doDec acc s@(c1:c2:'=':'=':cs) = maybe
+                    (Fail acc s)
+                    (\ bs -> Final (acc ++ take 1 bs) cs)
+                    (dec4 [c1, c2, 'A', 'A'])
+                doDec acc s@(c1:c2:c3:'=':cs) = maybe
+                    (Fail acc s)
+                    (\ bs -> Final (acc ++ take 2 bs) cs)
+                    (dec4 [c1, c2, c3, 'A'])
+                doDec acc s@(c1:c2:c3:c4:cs) = maybe
+                    (Fail acc s)
+                    (\ bs -> doDec (acc ++ bs) cs)
+                    (dec4 [c1, c2, c3, c4])
+                doDec acc s = Part acc (dI s)
 
 -- | Decode data (strict).
 decode :: String
     -> Maybe [Word8]
-decode = sequence . decode'
+decode s = let
+        d = decodeInc (Chunk s)
+    in case d of
+        Final da _ -> Just da
+        Fail _ _ -> Nothing
+        Part da f -> let
+                d' = f Done
+            in case d' of
+                Final da' _ -> Just $ da ++ da'
+                Fail _ _ -> Nothing
+                Part _ _ -> Nothing -- should never happen
 
 -- {{{1 chop
 -- | Chop up a string in parts.
